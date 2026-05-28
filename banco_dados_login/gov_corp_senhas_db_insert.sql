6f2fe31dffc4ba803773d5c513832a6b997614ff
Alaskalx <alcantaranonato.1@gmail.com>
2026-05-28 13:36:02 -0300

    Add files via upload

---
+++ banco_dados_login/gov_corp_senhas_db_insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO login_auditoria_processos (username, senha_hash, perfil, primeiro_acesso, ativo)
+VALUES (
+    'admin',
+    'def',
+    'admin',
+    1,
+    1
+);
+
+INSERT INTO login_auditoria_processos (username, senha_hash, perfil, primeiro_acesso, ativo)
+VALUES ('padrão, 'def', usuario,1,1 );
